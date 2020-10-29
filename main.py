de24114b0113f230b32319b8512e8a95223ab3a0
sparvind2000 <sparvind2000@gmail.com>
2020-10-29 19:17:18 +0530

    add 'conflict msg'

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('This a remote master change test for PyCharm')
+    print_hi('This a remote master change test for PyCharm') #local change
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
